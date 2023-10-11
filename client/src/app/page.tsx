76f2e20065415ff049bd79796fea7b50d10dfa5f
Rafaric <rafaelstrongoli@gmail.com>
2023-10-11 18:48:51 -0300

    readecuado componente navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,6 @@
-import Navbar from "../components/Navbar/Navbar";
 export default function Home() {
   return (
     <main className="flex h-screen items-center flex-col">
-      <Navbar />
       <div>Hola mundo</div>
     </main>
   );
