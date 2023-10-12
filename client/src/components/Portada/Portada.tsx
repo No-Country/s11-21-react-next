e31d1da5ea459b6504b940bb43f736e29048c6d7
Rafaric <rafaelstrongoli@gmail.com>
2023-10-12 19:00:07 -0300

    corregido Image portada.

--- client/src/components/Portada/Portada.tsx
+++ client/src/components/Portada/Portada.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import SearchInput from '../Searcher/searchInput'
+import React from "react";
+import SearchInput from "../Searcher/searchInput";
 import Image from "next/image";
 
 export default function Portada() {
   return (
     <>
-      <div className='relative flex justify-center items-center -z-10 w-2/4 max-md:w-full -mt-6e'>
-        <Image src='/Portada.jpg' alt="image-front-page" className=' object-contain brightness-75 w-fit' height={500} width={500}/>
-        <span className='absolute bottom-16 left-10'>
-          <h1 className='text-[#fff] text-xl font-extralight'>NearByTour</h1>
-          <p className=' text-lg text-[#FFFFFF] w-72 '>Comparte y descubre lugares únicos en todo el mundo.</p>
+      <div className="relative h-[260px] flex justify-center items-center -z-10 w-2/4 max-md:w-full -mt-6">
+        <Image
+          src="/Portada.jpg"
+          alt="image-front-page"
+          className="brightness-75"
+          fill
+          objectFit="cover"
+        />
+        <span className="absolute bottom-16 left-10">
+          <h1 className="text-[#fff] text-xl font-extralight">NearByTour</h1>
+          <p className=" text-lg text-[#FFFFFF] w-72 ">
+            Comparte y descubre lugares únicos en todo el mundo.
+          </p>
         </span>
       </div>
       <SearchInput></SearchInput>
     </>
-  )
+  );
 }
