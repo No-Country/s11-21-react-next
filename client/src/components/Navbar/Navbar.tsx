1dc2612039efdb90bd98f7d85f95125375786dce
Rafaric <rafaelstrongoli@gmail.com>
2023-10-12 18:40:05 -0300

    corregido logo por nombre

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,6 +4,9 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 import Menu from "./Menu";
 import Image from "next/image";
+import { Krona_One } from "next/font/google";
+
+const krona = Krona_One({ weight: "400", subsets: ["latin"] });
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -12,7 +15,10 @@ const Navbar = () => {
   };
   return (
     <nav className="bg-[#FD7B03] w-screen text-neutral-200 h-14 flex items-center justify-between p-6">
-      <Image src="/LOGO_NT-01 1.png" alt="logo" width={50} height={50} />
+      {/* <Image src="/LOGO_NT-01 1.png" alt="logo" width={50} height={50} /> */}
+      <h1 className={`${krona.className} text-[18px] text-white`}>
+        NearByTour
+      </h1>
       {!open ? (
         <GiHamburgerMenu size={25} color="white" onClick={openMenu} />
       ) : (
