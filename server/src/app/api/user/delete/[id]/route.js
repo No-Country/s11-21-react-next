9287b374f5e4fecc3ba061bfb1f862b45ad21203
PabloMur <polillomurillo@gmail.com>
2023-10-13 20:09:58 -0300

    cambios de version y arquitectura

--- server/src/app/api/user/delete/[id]/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { UserModel } from "@/models/User";
-
-export async function DELETE(request, { params }) {
-    const idUser = await UserModel.deleteUser(params.id);
-    return NextResponse.json('Delete userDB: '+ JSON.stringify( idUser ) );
-}
--- server/src/app/api/user/update/[id]/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { UserModel } from "@/models/User";
-
-export async function PUT(request, { params }) {
-    const body = await request.json();
-    const { updateData } = body;
-    const updateUser = await UserModel.updateUser(params.id, updateData);
-    return NextResponse.json('Update user: '+ JSON.stringify( updateUser ) );
-}
