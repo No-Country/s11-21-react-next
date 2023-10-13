15d91ff96ae613e04a96c615829cc761520f720f
Ash19-88 <ayelenllampa88@gmail.com>
2023-10-12 23:42:11 -0300

    carousels ready

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,6 +3,7 @@ import CarouselCard from "@/components/Carousel/CarouselCard";
 import Populars from "@/components/Carousel/Headers/Populars";
 import BestRated from "@/components/Carousel/Headers/BestRated";
 import SearchResultCard from "@/components/card/SearchResultCard";
+import Recently from "@/components/Carousel/Headers/Recently";
 
 export default function Home() {
   return (
@@ -16,17 +17,7 @@ export default function Home() {
       <div className="flex overflow-x-auto overflow-hidden">
         <CarouselCard />
       </div>
-      <div className="flex flex-row justify-between mt-4">
-        <div className="pl-8">
-          <h3
-            className="text-black font-semibold 
-        "
-          >
-            Vistos recientemente
-          </h3>
-        </div>
-        <button className="text-[#FD7B03] flex pr-6">Ver más</button>
-      </div>
+      <Recently />
       <div className="flex overflow-x-auto overflow-hidden">
         <CarouselCard />
       </div>
