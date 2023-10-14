a85aeaa1c4a367ae05c5048802c5ad15e2adec3b
OLuciani <lucianioscar1@gmail.com>
2023-10-13 21:48:32 -0300

    Se agrandó y se hicieron correcciones en la card (Resultado de búsqueda, componente SearchResultCar.tsx)

--- client/src/components/card/SearchResultCard.tsx
+++ client/src/components/card/SearchResultCard.tsx
@@ -21,13 +21,13 @@ const SearchResultCard = () => {
 
   return (
     <div className="grid my-5">
-      <div className="relative w-[290px] h-[80px] bg-[#FFF4E0] justify-self-center rounded-lg box-border flex gap-[5px]">
-        <div className="w-[113px] h-[70px] mt-[5px] ml-[5px]">
-          <Image src={CardImage} alt="Card image" priority className="w-[113px] h-[70px] rounded-[3px] items-center" />
+      <div className="relative w-[348px] h-[96px] bg-[#FFF4E0] justify-self-center rounded-lg box-border flex gap-[5px]">
+        <div className="w-[136px] h-[84px] mt-[6px] ml-[6px] mb-[6px]">
+          <Image src={CardImage} alt="Card image" priority className="w-[136px] h-[84px] rounded-md items-center" />
         </div>
 
-        <div className="w-[135px] h-[70px] mt-[5px] ml-[5px] text-[14px] relative">
-          <div className="text-left flex flex-col gap-1">
+        <div className="w-[162px] m-auto text-[14px] relative">
+          <div className="text-left flex flex-col gap-2">
             <div className="relative">
               <p
                 className={`truncate ${isHovered ? '' : 'truncate'}`}
@@ -41,7 +41,7 @@ const SearchResultCard = () => {
             <div className="flex gap-2 items-center">
               <Image src={MapIconImage} alt="Map-icon image" className="h-[12px]" />
               <Link href="https://www.google.com/maps/place/Iglesia+del+Sagrado+Coraz%C3%B3n+de+Jes%C3%BAs+(Iglesia+de+los+Capuchinos)/@-31.4245554,-64.188475,17z/data=!3m1!4b1!4m6!3m5!1s0x9432a28f9b82eb4f:0x5fa116c0073e51e2!8m2!3d-31.42456!4d-64.1859001!16s%2Fg%2F1225dx4w?entry=ttu">
-                <p>Como llegar</p>
+                <p className='text-blue-600 hover:text-base hover:font-semibold'>Como llegar</p>
               </Link>
             </div>
 
@@ -67,15 +67,15 @@ const SearchResultCard = () => {
             </div>
           </div>
           {isHovered && (
-            <div className="absolute top-0 left-0 w-full h-full bg-white p-1 z-10">
+            <div className="absolute top-0 left-0 w-full h-full bg-white p-1 z-10 border-[2px] rounded-lg">
               <p className="text-left text-[12px]">Iglesia Jesuitica Los Capuchinos. Ciudad de Córdoba. </p>
             </div>
           )}
         </div>
 
-        <div className="w-[15px] h-[70px] mt-[5px] flex flex-col">
-          <div className="h-[21px] grid items-center justify-center">
-            <Image src={HeartImage} alt="Map-icon image" className="w-[11.25px] h-[10px]" />
+        <div className="w-[22px] h-[84px] mt-[7px] flex flex-col">
+          <div className="h-[25px] grid items-center justify-left">
+            <Image src={HeartImage} alt="Heart image" width={14.5} />
           </div>
         </div>
       </div>
