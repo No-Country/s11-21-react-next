8a6ef7f1a483c7dad388759c06fabd0618f3c540
Ash19-88 <ayelenllampa88@gmail.com>
2023-10-26 01:18:11 -0300

    description, review, touristPlaceCard responsive

--- client/src/app/(otherPages)/place/page.tsx
+++ client/src/app/(otherPages)/place/page.tsx
@@ -1,17 +1,16 @@
 import OpinionCard from "@/components/OpinionCard/OpinionCard";
 import TouristPlaceCard from "@/components/TouristPlaceCard/TouristPlaceCard";
 import OpinionsResume from "@/components/Opinions/OpinionsResume";
-import Description from "@/components/Description/page";
-
 import React from "react";
 import Review from "@/components/FormReview/Review";
+import Description from "@/components/Description/Description";
 
 const Place = () => {
   return (
     <div className="flex flex-col items-center h-auto overflow-y-scroll w-full">
       <TouristPlaceCard />
-      <Description/>
-      <Review></Review>
+      <Description />
+      <Review />
       <OpinionsResume />
       <OpinionCard />
     </div>
--- client/src/components/FormReview/Review.tsx
+++ client/src/components/FormReview/Review.tsx
@@ -1,8 +1,6 @@
-"use client"
-import React, { useState } from 'react'
-import { AiOutlineDownload } from 'react-icons/ai'
-import { RiStarSFill } from 'react-icons/ri'
-
+"use client";
+import React, { useState } from "react";
+import { RiStarSFill } from "react-icons/ri";
 
 export default function Review() {
   const renderStars = () => {
@@ -15,9 +13,8 @@ export default function Review() {
     return stars;
   };
 
-  
   const [hoveredStars, setHoveredStars] = useState(Array(5).fill(false));
-  const handleHover = (index:number) => {
+  const handleHover = (index: number) => {
     const newHoveredStars = [...hoveredStars];
     for (let i = 0; i < newHoveredStars.length; i++) {
       newHoveredStars[i] = i <= index;
@@ -26,22 +23,32 @@ export default function Review() {
   };
 
   return (
-    <div className='m-auto w-4/5 h-auto py-8 text-[#000] border-t-2 border-[#FFCF91]'>
-      <h1 className='font-semibold'>¿Te gustaría dar tu opinión del lugar?</h1>
-      <h2 className=' font-semibold text-sm'>¿Cómo fue tu experiencia?</h2>
-      <span className='flex my-1'>
-      {hoveredStars.map((isHovered, index) => (
-        <RiStarSFill
-          className={`text-${isHovered ? '[#FD7B03]' : '[#FFCF91]'} hover:text-[#FD7B03] text-lg`}
-          key={index}
-          onMouseEnter={() => handleHover(index)}
-          onMouseLeave={() => handleHover(-1)}
-        />
-      ))}
-      </span>
-      <p className='font-semibold'>Escribe tu opinión</p>
-      <textarea className=' border-2 border-[#777] p-4 w-full h-32 rounded-lg mt-2  max-h-60' ></textarea>
-      <button className='bg-[#FD7B03] w-full h-12 mt-2 rounded-lg text-[#fff] font-semibold'>Enviar opinión</button>
+    <div className="w-11/12 xl:w-3/5 h-auto px-6 text-[#000]  justify-center">
+      <div className="w-full px-2 border-t-2 border-[#FFCF91] py-8">
+        <h1 className="font-semibold">
+          ¿Te gustaría dar tu opinión del lugar?
+        </h1>
+        <h2 className=" font-semibold text-sm">¿Cómo fue tu experiencia?</h2>
+        <span className="flex my-1">
+          {hoveredStars.map((isHovered, index) => (
+            <RiStarSFill
+              className={`text-${
+                isHovered ? "[#FD7B03]" : "[#FFCF91]"
+              } hover:text-[#FD7B03] text-lg`}
+              key={index}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(-1)}
+            />
+          ))}
+        </span>
+        <p className="font-semibold">Escribe tu opinión</p>
+        <textarea className=" border-2 border-[#777] p-4 w-full h-32 rounded-lg mt-2  max-h-60"></textarea>
+        <div className="flex justify-center items-center">
+          <button className="bg-[#FD7B03] w-full lg:w-2/4   h-12 mt-2 rounded-lg text-[#fff] font-semibold">
+            Enviar opinión
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
