9d76a74034515db5db752792a2fc53a3c228bb45
OLuciani <lucianioscar1@gmail.com>
2023-10-30 16:18:52 -0300

    Lógica para modificar datos del componente UserProfile.tsx. Responsive del mismo componente para tablet y Desktop

--- client/src/context/Context.tsx
+++ client/src/context/Context.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 import React, { createContext, useState, ReactNode } from "react";
 
 interface ContextProps {
@@ -31,6 +31,50 @@ export default function ContextProvider({ children }: { children: ReactNode }) {
 
   return (
     <Context.Provider value={{ showIconEdit, setShowIconEdit, showInformation, setShowInformation, textButton, setTextButton, userId, setUserId}}>
+      {children}
+    </Context.Provider>
+  );
+} */
+
+
+"use client"
+import React, { createContext, useState, ReactNode } from "react";
+
+interface ContextProps {
+  showIconEdit: string;
+  setShowIconEdit: React.Dispatch<React.SetStateAction<string>>;
+  showInformation: string;
+  setShowInformation: React.Dispatch<React.SetStateAction<string>>;
+  textButton: string;
+  setTextButton: React.Dispatch<React.SetStateAction<string>>;
+  userId: string;
+  setUserId: React.Dispatch<React.SetStateAction<string>>;
+  showErrorMessage: string;
+  setShowErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const Context = createContext<ContextProps>({
+    showIconEdit: "hidden",
+    setShowIconEdit: () => {},
+    showInformation: "block",
+    setShowInformation: () => {},
+    textButton: "Editar perfil",
+    setTextButton: () => {},
+    userId: "",
+    setUserId: () => {},
+    showErrorMessage: "",
+    setShowErrorMessage: () => {},
+});
+
+export default function ContextProvider({ children }: { children: ReactNode }) {
+  const [showIconEdit, setShowIconEdit] = useState<string>("hidden");
+  const [showInformation, setShowInformation] = useState<string>("block");
+  const [textButton, setTextButton] = useState<string>("Editar perfil");
+  const [userId, setUserId] = useState<string>("");
+  const [showErrorMessage, setShowErrorMessage] = useState<string>("");
+
+  return (
+    <Context.Provider value={{ showIconEdit, setShowIconEdit, showInformation, setShowInformation, textButton, setTextButton, userId, setUserId, showErrorMessage, setShowErrorMessage}}>
       {children}
     </Context.Provider>
   );
