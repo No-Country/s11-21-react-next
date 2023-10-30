698ea891023a4cb573d46aee249bd1abc50e3f84
FrancoNos <fat7_2014@hotmail.com>
2023-10-30 15:42:34 -0300

    home

--- client/src/app/(otherPages)/home/page.tsx
+++ client/src/app/(otherPages)/home/page.tsx
@@ -1,25 +1,96 @@
+"use client";
+
+
+import React, { useEffect, useState } from "react";
 import Portada from "@/components/Portada/Portada";
 import CarouselCard from "@/components/Carousel/CarouselCard";
-import Populars from "@/components/Carousel/Headers/Populars";
 import BestRated from "@/components/Carousel/Headers/BestRated";
 import Recently from "@/components/Carousel/Headers/Recently";
 import Formulario from "@/components/AlFormulario/AlFormulario";
+import axios from "axios";
+import Populars from "@/components/Carousel/Headers/Populars";
+
+
+interface Place {
+  placeId: string;
+  data: {
+    website: string;
+    comments: any[]; 
+    stars: number;
+    category: string;
+    description: string;
+    imageUrl: string[];
+    placeName: string;
+    zone: string;
+    socialNetworks: { instagram: string }[];
+    views: number;
+    longitude: string;
+    latitude: string;
+    createdBy: string;
+  };
+}
+
+type CarouselPlace = {
+  id: number;
+  placeName: string;
+  imageUrl: string[];
+  zone: string;
+  stars: number;
+};
+
+
+const convertToCarouselPlace = (place: Place): CarouselPlace => {
+  return {
+    id: parseInt(place.placeId),
+    placeName: place.data.placeName,
+    imageUrl: place.data.imageUrl,
+    zone: place.data.zone,
+    stars: place.data.stars,
+  };
+};
 
 export default function Home() {
+  const [popularPlaces, setPopularPlaces] = useState<CarouselPlace[]>([]);
+  const [bestRatedPlaces, setBestRatedPlaces] = useState<CarouselPlace[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://nearby-back.vercel.app/api/place/mostPopulars")
+      .then((response) => {
+     
+        const convertedPlaces = response.data.results.map(convertToCarouselPlace);
+        setPopularPlaces(convertedPlaces);
+      })
+      .catch((error) => {
+        console.error("Error al cargar los lugares populares", error);
+      });
+
+    axios
+      .get("https://nearby-back.vercel.app/api/place/mostRated")
+      .then((response) => {
+        
+        const convertedPlaces = response.data.results.map(convertToCarouselPlace);
+        setBestRatedPlaces(convertedPlaces);
+      })
+      .catch((error) => {
+        console.error("Error al cargar los lugares mejor evaluados", error);
+      });
+  }, []);
+
   return (
     <main className="flex flex-col h-auto">
       <Portada />
-      <Populars />
+      <Populars popularPlaces={popularPlaces} />
       <div className="flex overflow-x-auto overflow-hidden">
-        <CarouselCard />
+      <CarouselCard places={popularPlaces} />
       </div>
       <BestRated />
       <div className="flex overflow-x-auto overflow-hidden">
-        <CarouselCard />
+      <CarouselCard places={bestRatedPlaces} />
       </div>
       <Recently />
       <div className="flex overflow-x-auto overflow-hidden">
-        <CarouselCard />
+        <CarouselCard places={bestRatedPlaces} />
       </div>
       <Formulario />
     </main>
--- client/src/app/(otherPages)/place/page.tsx
+++ client/src/app/(otherPages)/place/page.tsx
@@ -1,18 +1,47 @@
-import OpinionCard from "@/components/OpinionCard/OpinionCard";
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import TouristPlaceCard from "@/components/TouristPlaceCard/TouristPlaceCard";
-import OpinionsResume from "@/components/Opinions/OpinionsResume";
-import React from "react";
-import Review from "@/components/FormReview/Review";
 import Description from "@/components/Description/Description";
+import Review from "@/components/FormReview/Review";
+import OpinionsResume from "@/components/Opinions/OpinionsResume";
+import OpinionCard from "@/components/OpinionCard/OpinionCard";
+import axios from 'axios';
 
 const Place = () => {
+  const router = useRouter();
+  const { id } = router.query; // Obtiene el parámetro 'id' de la URL
+
+  const [placeData, setPlaceData] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      // Realiza una solicitud GET para obtener los datos del lugar con 'id'
+      axios.get(`https://nearby-back.vercel.app/api/place/getPlace?placeId=${id}`)
+        .then((response) => {
+          // Establece los datos del lugar en 'placeData'
+          setPlaceData(response.data);
+        })
+        .catch((error) => {
+          console.error('Error al obtener los datos del lugar', error);
+        });
+    }
+  }, [id]);
+
   return (
     <div className="flex flex-col items-center h-auto w-full">
-      <TouristPlaceCard />
-      <Description />
-      <Review />
-      <OpinionsResume />
-      <OpinionCard />
+      {placeData ? (
+        <>
+          <TouristPlaceCard data={placeData} />
+          <Description data={placeData} />
+          <Review data={placeData} />
+          <OpinionsResume data={placeData} />
+          <OpinionCard data={placeData} />
+        </>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
     </div>
   );
 };
