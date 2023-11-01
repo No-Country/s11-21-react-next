48876164110a175b3b12746496b4916928f6d84d
FrancoNos <fat7_2014@hotmail.com>
2023-11-01 00:14:20 -0300

    description

--- client/src/app/(otherPages)/place/[id]/page.tsx
+++ client/src/app/(otherPages)/place/[id]/page.tsx
@@ -1,28 +1,59 @@
 "use client"
 
-import OpinionCard from "@/components/OpinionCard/OpinionCard";
+import React, { useEffect, useState } from "react";
 import TouristPlaceCard from "@/components/TouristPlaceCard/TouristPlaceCard";
-import OpinionsResume from "@/components/Opinions/OpinionsResume";
-import React from "react";
-import Review from "@/components/FormReview/Review";
 import Description from "@/components/Description/Description";
-import { useRouter } from "next/navigation";
-
+import Review from "@/components/FormReview/Review";
+import OpinionsResume from "@/components/Opinions/OpinionsResume";
+import OpinionCard from "@/components/OpinionCard/OpinionCard";
+import { useParams } from "next/navigation";
+import axios from "axios";
+interface PlaceData {
+  description: string;
+  zone: string;
+  openingHours: string;
+}
 
 const Place = () => {
- 
+  const params = useParams();
+  const { id } = params;
+  const [placeData, setPlaceData] = useState<PlaceData | null>(null);
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`https://nearby-back.vercel.app/api/place/getPlace?placeId=${id}`)
+        .then((response) => {
+          console.log('Respuesta de la solicitud:', response.data);
+          setPlaceData(response.data.placeData);
+        })
+        .catch((error) => {
+          console.error('Error al cargar datos del lugar', error);
+        });
+    }
+  }, [id]);
+
   return (
     <div className="flex flex-col items-center h-auto w-full">
-        <>
-          <TouristPlaceCard  />
-          <Description  />
-          <Review />
-          <OpinionsResume />
-          <OpinionCard />
-        </>
-     
+      {placeData ? (
+       <>
+       <TouristPlaceCard  />
+       <Description
+         description={placeData.description}
+         zone={placeData.zone}
+       />
+       <Review  />
+       <OpinionsResume  />
+       <OpinionCard  />
+     </>
+      ) : (
+        <p>Cargando datos del lugar...</p>
+      )}
     </div>
   );
 };
 
 export default Place;
+
+
+ 
--- client/src/components/Description/Description.tsx
+++ client/src/components/Description/Description.tsx
@@ -2,31 +2,30 @@ import React from "react";
 import { FiMapPin } from "react-icons/fi";
 import { LuClock3 } from "react-icons/lu";
 
-export default function Description() {
+interface DescriptionProps {
+  description: string;
+  zone: string;
+  
+}
+
+const Description: React.FC<DescriptionProps> = ({ description, zone }) => {
   return (
-    <div className="w-11/12 2xl:w-1/2 text-[#000] justify-center">
+    <div className="w-11/12 2xl-w-1/2 text-[#000] justify-center">
       <div className="w-full justify-self-center flex justify-between px-6 mb-3">
         <div className="py-4 text-left">
           <h2 className="text-sm font-bold text-black mb-2">Descripción</h2>
 
           <p className="text-xs lg:text-sm xl:text-base text-justify text-black flex items-center justify-center rounded-full">
-            El Jardín Japonés en Buenos Aires es una auténtica joya que merece
-            ser explorada en detalle. Con una impresionante fusión de la
-            estética japonesa y la naturaleza exuberante, este lugar ofrece un
-            retiro tranquilo y cautivador en medio de la ciudad. ¡No te lo
-            pierdas!
+            {description}
           </p>
           <br />
 
           <div className="text-xs lg:text-sm flex flex-row">
             <FiMapPin className="font-medium text-base lg:text-lg mr-2 text-[#FD7B03]" />
-            <p
-              className="text-[#FD7B03]
-            font-medium leading-4"
-            >
+            <p className="text-[#FD7B03] font-medium leading-4">
               Dirección:
               <span className="text-black ml-2 text-justify font-normal">
-                Avenida Casares 3450, Buenos Aires, Argentina.
+                {zone}
               </span>
             </p>
           </div>
@@ -35,13 +34,15 @@ export default function Description() {
 
           <p className="text-xs lg:text-sm flex items-center">
             <span className="mr-2 text-[#FD7B03]">
-              <LuClock3 className="font-medium text-base lg:text-lg" />
+              <LuClock3 className="font-medium text-base lg-text-lg" />
             </span>
             <span className="text-[#FD7B03] font-medium">Horario:</span>{" "}
-            <span className="text-black ml-2">10:00 - 18:30 </span> hrs.
+            <span className="text-black ml-2">horario</span>
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Description;
