50ab0bc897704223a0a61939ac3320a9353f84de
Ash19-88 <ayelenllampa88@gmail.com>
2023-11-02 17:00:36 -0300

    headers listos

--- client/src/components/Carousel/Headers/Recently.tsx
+++ client/src/components/Carousel/Headers/Recently.tsx
@@ -3,21 +3,25 @@ import React from "react";
 
 const Recently = () => {
   return (
-    <div className="flex flex-row justify-between mt-4">
-      <div className="pl-8">
-        <h3
-          className="text-black font-semibold 
+    <div className="mx-0 lg:mx-[13vw] xl:mx-[30vw] 2xl:mx-[32vw]">
+      <div className="flex flex-row justify-between mt-4">
+        <div className="pl-8 lg:pl-0 xl:pl-0 text-start">
+          <h3
+            className="text-black font-semibold 
         "
-        >
-          Vistos recientemente
-        </h3>
+          >
+            Vistos recientemente
+          </h3>
+        </div>
+        <div className="text-end">
+          <Link
+            href="/recentViewed"
+            className="text-[#FD7B03] text-[12px] flex pr-6"
+          >
+            Ver más
+          </Link>
+        </div>
       </div>
-      <Link
-        href="/recentViewed"
-        className="text-[#FD7B03] text-[12px] flex pr-6"
-      >
-        Ver más
-      </Link>
     </div>
   );
 };
